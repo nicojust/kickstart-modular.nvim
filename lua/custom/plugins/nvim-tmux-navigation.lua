5375075e1c3d99798259a7fabc445deba453db5f
Nekwo <just.nico@yahoo.de>
2024-03-28 09:35:52 +0100

    feat(keymaps): add keymap descriptions

--- lua/custom/plugins/nvim-tmux-navigation.lua
+++ lua/custom/plugins/nvim-tmux-navigation.lua
@@ -10,11 +10,11 @@ return {
             disable_when_zoomed = true -- defaults to false
         }
 
-        vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-        vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-        vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-        vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-        vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-        vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+        vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft, { desc = 'Tmux navigate left' })
+        vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown, { desc = 'Tmux navigate down' })
+        vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp, { desc = 'Tmux navigate up' })
+        vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight, { desc = 'Tmux navigate right' })
+        vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive, { desc = 'Tmux navigate to last active' })
+        vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext, { desc = 'Tmux navigate to next' })
     end
 }

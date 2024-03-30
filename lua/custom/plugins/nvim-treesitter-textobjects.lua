1a5f58a1f36bcc4459a0fea0fe3facaaca01a5ce
Nekwo <just.nico@yahoo.de>
2024-03-30 09:21:44 +0100

    feat(plugins): add optional treesitter plugins

---
+++ lua/custom/plugins/nvim-treesitter-textobjects.lua
@@ -0,0 +1,9 @@
+-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+-- nvim-treesitter-textobjects plugin configuration
+
+return {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    }
+}

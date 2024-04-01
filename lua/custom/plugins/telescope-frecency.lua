9e4f0d8667cd4ff79100995f857d836ae64b90e5
Nekwo <just.nico@yahoo.de>
2024-04-01 14:33:58 +0200

    feat(plugins): add a couple more plugins
    mostly telescope and colorschemes

---
+++ lua/custom/plugins/telescope-frecency.lua
@@ -0,0 +1,8 @@
+-- https://github.com/nvim-telescope/telescope-frecency.nvim
+
+return {
+    "nvim-telescope/telescope-frecency.nvim",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+    }
+}

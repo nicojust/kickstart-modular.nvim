2212d45859907b7e9c93d61ba9d114f5c82b4e5b
Nekwo <just.nico@yahoo.de>
2024-04-28 21:43:11 +0200

    feat(noice): update config

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -3,8 +3,32 @@
 return {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {},
     dependencies = {
-        "MunifTanjim/nui.nvim"
-    }
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify"
+    },
+    config = function()
+        local noice = require("noice")
+
+        noice.setup({
+            lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                },
+            },
+            -- you can enable a preset for easier configuration
+            presets = {
+                command_palette = true,       -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false,       -- add a border to hover docs and signature help
+            },
+        })
+
+        vim.keymap.set("n", "<leader>nl", function() noice.cmd("last") end, { desc = '[n]oice [l]ast message' })
+        vim.keymap.set("n", "<leader>nh", function() noice.cmd("history") end, { desc = '[n]oice message [h]istory' })
+    end
 }

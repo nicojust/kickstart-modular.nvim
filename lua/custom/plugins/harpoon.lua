d5c0af96d1d3652c308a5e92f89fa189d372bec2
Nekwo <just.nico@yahoo.de>
2024-05-15 02:22:53 +0200

    feat(harpoon): simplify keymaps

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -4,9 +4,6 @@
 return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
     config = function()
         local harpoon = require("harpoon")
         harpoon:setup({})
@@ -14,13 +11,13 @@ return {
         vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = 'Add to harpoon' })
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Show harpoon' })
 
-        vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end, { desc = 'Open harpoon buffer 1' })
-        vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = 'Open harpoon buffer 2' })
-        vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end, { desc = 'Open harpoon buffer 3' })
-        vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = 'Open harpoon buffer 4' })
+        -- Set <leader>h1..<leader>h5 as shortcuts for selecting files
+        for _, idx in ipairs { 1, 2, 3, 4, 5 } do
+            vim.keymap.set("n", string.format("<leader>h%d", idx), function() harpoon:list():select(idx) end, { desc = string.format("harpoon buffer [%d]", idx) })
+        end
 
         -- Toggle previous & next buffers stored within Harpoon list
-        vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end, { desc = 'Open [p]revious harpoon buffer' })
-        vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end, { desc = 'Open [n]ext harpoon buffer' })
+        vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end, { desc = '[p]revious harpoon buffer' })
+        vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end, { desc = '[n]ext harpoon buffer' })
     end,
 }

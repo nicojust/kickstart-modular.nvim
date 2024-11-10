ae17b6015a76b0260d1a0ef05e95aa69018d6142
Nekwo <just.nico@yahoo.de>
2024-11-10 13:03:26 +0100

    feat(plugin): add plugins from lazyvim

---
+++ lua/custom/plugins/grug.lua
@@ -0,0 +1,12 @@
+-- https://github.com/MagicDuck/grug-far.nvim#-installation
+
+return {
+    'MagicDuck/grug-far.nvim',
+    config = function()
+        require('grug-far').setup({
+            -- options, see Configuration section below
+            -- there are no required options atm
+            -- engine = 'ripgrep' is default, but 'astgrep' can be specified
+        });
+    end
+}

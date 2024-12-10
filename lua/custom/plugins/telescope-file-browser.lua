695cb1e8acf75df56f59aa12ddb97da1f4d4825a
Nekwo <just.nico@yahoo.de>
2024-12-10 19:46:12 +0100

    feat(telescope): remove file browser ext

--- lua/custom/plugins/telescope-file-browser.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/nvim-telescope/telescope-file-browser.nvim
-
-return {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = {
-        "nvim-telescope/telescope.nvim",
-        "nvim-lua/plenary.nvim"
-    }
-}

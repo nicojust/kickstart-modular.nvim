d3ca6170fcacab0479502bd0fa096c0512a54c84
Nekwo <just.nico@yahoo.de>
2024-12-14 15:40:34 +0100

    feat(telescope): add smart history extension

---
+++ lua/custom/plugins/telescope-smart-history.lua
@@ -0,0 +1,9 @@
+-- https://github.com/nvim-telescope/telescope-smart-history.nvim
+
+return {
+    "nvim-telescope/telescope-smart-history.nvim",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "kkharji/sqlite.lua",
+    }
+}
